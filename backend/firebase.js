a6a38357e603c96cf41e00f3179662f75d549919
Axel Lenin Pullaguari Cedeño <150882583+ALPullaguariSW@users.noreply.github.com>
2024-02-07 22:40:14 -0500

    Add backend fire
    Agree

---
+++ backend/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6jMNmURDRVCsFHFv7JC2tOWIT0Z1SIn8",
+  authDomain: "autenticador-8fd98.firebaseapp.com",
+  databaseURL: "https://autenticador-8fd98-default-rtdb.firebaseio.com",
+  projectId: "autenticador-8fd98",
+  storageBucket: "autenticador-8fd98.appspot.com",
+  messagingSenderId: "599275193091",
+  appId: "1:599275193091:web:21d71db638c5fb8539df36",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
